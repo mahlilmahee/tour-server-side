28c92b07d326204ad07630f6f87cf8164b93b7d9
acc username <acc email>
2021-10-30 23:09:26 +0600

    add

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 4000;
+const port =process.env.PORT || 4000;
 const { MongoClient } = require('mongodb');
-
+const cors=require('cors')
 require('dotenv').config()
 // password \  assignment
 // YCbvwOMwF71TNfKU
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ufugb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -17,21 +19,18 @@ async function run() {
       await client.connect();
   
       const database = client.db("insertDB");
-  
       const UserInformation = database.collection("Information");
-  
       // create a document to insert
+     app.get('/tour', async(req,res)=>{
+        const query={}
+        const cursor = UserInformation.find(query);
+        const result= await cursor.toArray();
+        console.log('I have got this ')
+        res.json(result)
+     })
+      
   
-      const doc = {
-
-           title: "Record of a Shriveled Datum",
-           content: "No bytes, no problem. Just insert a document, in MongoDB",
-
-      }
   
-      const result = await UserInformation.insertOne(doc);
-  
-      console.log(`A document was inserted with the _id: ${result.insertedId}`);
   
     } finally {
   
