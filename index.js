a30447325d1d4a562f6fca10b2351ebc819c7139
acc username <acc email>
2021-10-31 13:44:43 +0600

    added

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const port =process.env.PORT || 4000;
 const { MongoClient } = require('mongodb');
-const cors=require('cors')
+const cors=require('cors');
+const ObjectId=require('mongodb').ObjectId;
 require('dotenv').config()
 // password \  assignment
 // YCbvwOMwF71TNfKU
@@ -20,6 +21,8 @@ async function run() {
   
       const database = client.db("insertDB");
       const UserInformation = database.collection("Information");
+      const UserInformationstored = database.collection("storage");
+      
       // create a document to insert
      app.get('/tour', async(req,res)=>{
         const query={}
@@ -28,8 +31,34 @@ async function run() {
         console.log('I have got this ')
         res.json(result)
      })
+    //  taking information for my orders 
+
+     app.get('/tourinfo/:userEmail', async(req,res)=>{
+       const userEmail=req.params.userEmail;
+        const query= { email: {userEmail} };
+        const cursor = UserInformationstored.find(query);
+        const result= await cursor.toArray();
+        console.log('I have got this ', userEmail)
+        res.json(result);
+     })
+    //  new response for orderdetails here 
+    app.get('/tour/:tourId',async(req,res)=>{
+      const userOrder=req.params.tourId;
+      const query={_id:ObjectId(userOrder)};
+      const result=await UserInformation.findOne(query);
+      // console.log(userOrder,result);
+      res.json(result)
+    })
+    // taking tour packages informatation from the user here 
+
+    app.post('/tourinfo',async(req,res)=>{
+      const userInfo=req.body;
+      const result=await UserInformationstored.insertOne(userInfo);
+      console.log(userInfo)
+      res.send(result)
       
-  
+    })
+      
   
   
     } finally {
